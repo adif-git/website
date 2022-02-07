343fa82b87e705563bce615ad8635d5d94b1171d
adif-git <adif.ladiva@gmail.com>
2022-02-07 23:50:22 +0700

    Update footer styles based on pathname

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 export default function Footer() {
+  const router = useRouter();
+
   return (
-    <div className="w-full h-18 bg-slate-900 text-slate-500 px-20 py-10">
+    <div
+      className={`w-full h-18 px-20 py-7 ${
+        router.pathname === '/'
+          ? 'bg-slate-900 text-slate-500 border-t-2 border-slate-800'
+          : 'bg-slate-300 text-slate-500'
+      }`}
+    >
       <p className="font-bold text-lg text-right">Created by: Adif, 2022</p>
     </div>
   );
