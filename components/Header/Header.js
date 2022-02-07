bd4768017a8b376036c5e607bb3c077bd66b165e
adif-git <adif.ladiva@gmail.com>
2022-02-07 23:35:26 +0700

    Change logo icon

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import path from '../path';
 import { useRouter } from 'next/router';
+import { ChipIcon } from '@heroicons/react/outline';
 
 export default function Header() {
   const router = useRouter();
@@ -11,20 +12,7 @@ export default function Header() {
         <Link href={path.home}>
           <a className="flex justify-start h-28">
             <div className="h-full flex items-center mr-5">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-14 w-14 text-slate-50"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z"
-                />
-              </svg>
+              <ChipIcon className="h-14 w-14 text-slate-50" />
             </div>
             <div className="h-full flex items-center">
               <h1 className="text-slate-50 text-4xl font-bold mr-10">ADIF</h1>
