8215953217a4c441d44052323b45d7d1bc79c166
adif-git <adif.ladiva@gmail.com>
2022-02-28 21:38:15 +0700

    Update footer styles

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import { CgSmartphoneChip } from 'react-icons/cg';
+import { AiFillGithub, AiOutlineMail, AiFillLinkedin } from 'react-icons/ai';
+import Link from 'next/link';
+
 export default function Footer() {
   const router = useRouter();
 
+  const stylesIcon = `flex flex-row text-slate-500 hover:text-slate-50 transition ease-in-out ${
+    router.pathname === '/' || router.pathname === '/contacts'
+      ? 'hover:text-slate-50'
+      : 'hover:text-slate-900'
+  }`;
+
+  const stylesBG = `w-screen ${
+    router.pathname === '/' || router.pathname === '/contacts'
+      ? 'bg-slate-900 text-slate-500 border-t-2 border-slate-800'
+      : 'bg-slate-200 text-sky-900'
+  }`;
+
   return (
-    <div
-      className={`w-full h-18 px-20 py-7 ${
-        router.pathname === '/' || router.pathname === '/contacts'
-          ? 'bg-slate-900 text-slate-500 border-t-2 border-slate-800'
-          : 'bg-slate-200 text-blue-900'
-      }`}
-    >
-      <p className="font-bold text-lg text-right">Created by: Adif, 2022</p>
+    <div className={stylesBG}>
+      <div
+        className={`flex flex-row justify-between w-full h-18 px-5 py-6 max-w-7xl mx-auto`}
+      >
+        <div className="flex flex-row my-auto">
+          <CgSmartphoneChip size={20} className="mr-2" />
+          <div className="text-md font-bold mr-3">
+            Made by <span className="text-sky-500">Adif</span> with Next.js,
+            Tailwind, & Strapi @ 2022
+          </div>
+        </div>
+        <div className="flex flex-row my-auto">
+          <Link href="https://github.com/adif-git">
+            <a target="_blank" rel="noopener noreferrer">
+              <div className={stylesIcon}>
+                <div className="mx-3">
+                  <AiFillGithub size={20} />
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href="https://linkedin.com/in/ladivab">
+            <a target="_blank" rel="noopener noreferrer">
+              <div className={stylesIcon}>
+                <div className="mx-3">
+                  <AiFillLinkedin size={20} />
+                </div>
+              </div>
+            </a>
+          </Link>
+          <Link href="mailto:adif.ladiva@gmail.com">
+            <a target="_blank" rel="noopener noreferrer">
+              <div className={stylesIcon}>
+                <div className="mx-3">
+                  <AiOutlineMail size={20} />
+                </div>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
