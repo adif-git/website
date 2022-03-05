8da738ad2e46f57a91e118408cb2c01fcd83ecff
adif-git <adif.ladiva@gmail.com>
2022-03-05 19:23:54 +0700

    Add icon attributes

--- components/Profile/Skills.tsx
+++ components/Profile/Skills.tsx
@@ -1,17 +1,26 @@
 import { HiCode, HiChip, HiCollection, HiGlobeAlt } from 'react-icons/hi';
-import { SkillsProps } from '../../types/types';
+import Link from 'next/link';
 
+import { SkillsProps } from '../../types/types';
 import Skill from './Skill';
 
 const Skills: React.FC<SkillsProps> = ({ skills }) => {
   return (
     <>
-      <h1 className="leading-relaxed font-bold text-4xl text-center text-slate-800 mb-24">
+      <h1 className="leading-relaxed font-bold text-4xl text-center text-slate-800 mb-5">
         <span className="underline decoration-solid decoration-4 decoration-sky-500 underline-offset-8">
           Tech Stack
         </span>{' '}
         I Used & Currently Learning...
       </h1>
+      <p className="mb-20 w-full text-center font-semibold text-lg text-slate-600 italic">
+        Icons by{' '}
+        <Link href="https://icons8.com/">
+          <a className="text-sky-500" target="_blank" rel="noopener noreferrer">
+            icons8.com
+          </a>
+        </Link>
+      </p>
       <div className="flex flex-col md:flex-row flex-wrap justify-center ">
         {skills.map(({ attributes }, key) => {
           switch (attributes.name) {
