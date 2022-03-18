aa3d3adf635b4c29544c7edb29dd9089d4777bc4
adif-git <adif.ladiva@gmail.com>
2022-03-19 00:47:12 +0700

    Update header styling on mobile

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -10,7 +10,7 @@ const Header: React.FC = () => {
 
   return (
     <nav className="w-screen z-50 bg-slate-900 border-b-4 border-gray-800/50">
-      <div className="flex flex-row w-full max-w-7xl mx-auto px-5 py-7">
+      <div className="flex flex-row w-full max-w-7xl mx-auto py-3 md:px-5 md:py-7">
         <Link href={path.home}>
           <a className="flex justify-center md:justify-start mr-3 md:mr-10">
             <Logo />
