fb78dcd0d855b0c0ba3907c3b350a29e48094367
adif-git <adif.ladiva@gmail.com>
2022-04-20 22:10:57 +0700

    Change logo

--- components/Header/Logo.tsx
+++ components/Header/Logo.tsx
@@ -4,12 +4,12 @@ const Logo: React.FC = () => {
   return (
     <div className="flex md:translate-x-8 md:border-r-4 md:border-sky-400">
       <Image
-        src="/Logo.png"
+        src="/White-TP.svg"
         alt="..."
-        width={100}
+        width={90}
         height={50}
         objectFit="scale-down"
-        className="opacity-100 md:opacity-50 md:hover:opacity-100 md:hover:scale-125 ease-in duration-100"
+        className="opacity-70 md:opacity-50 md:hover:opacity-100 ease-in duration-100"
       />
     </div>
   );
